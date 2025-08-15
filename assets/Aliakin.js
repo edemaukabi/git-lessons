aba239866a618ef8549582aedfd0dfddcf73072b
ALI AKINFENWA <aliakinfenwa@ALIs-MacBook-Pro.local>
2025-08-15 20:19:01 +0100

    Aliakin.js

---
+++ assets/Aliakin.js
@@ -0,0 +1,11 @@
+const headers = [
+    {name: "Home", path: "/home", icon: "home"},
+    {name: "Profileset", path: "/profile", icon: "user"},
+    {name: "Settings", path: "/settings", icon: "cog"},
+]
+
+const footerLinks = [
+    {name: "NotPrivacy Policy", path: "/privacy"},
+    {name: "Terms of Service", path: "/terms"},
+    {name: "Contact Us", path: "/contact"},
+]
