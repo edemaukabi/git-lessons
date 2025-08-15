60143755f7da1266bd799c7f72e519cc5d3bf2b7
Admin <admin@Admins-MacBook-Pro.local>
2025-08-15 20:18:47 +0100

    picture and file added

---
+++ assets/tee.js
@@ -0,0 +1,11 @@
+const headers = [
+    {name: "Home", path: "/home", icon: "home"},
+    {name: "About Us", path: "/about", icon: "user"},
+    {name: "Settings", path: "/settings", icon: "cog"},
+]
+
+const footerLinks = [
+    {name: "Privacy Policy", path: "/privacy"},
+    {name: "Terms of Service", path: "/terms"},
+    {name: "Contact", path: "/contact"},
+]
